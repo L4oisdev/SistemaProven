06e1c763752c064248013aa461fab16bebc591c3
L4ois <luisfrarncorh@gmail.com>
2024-10-11 12:47:22 -0400

    Resolving conflicts3

--- SistemaProven.AplicacionWeb/obj/Debug/net8.0/SistemaProven.AplicacionWeb.AssemblyInfo.cs
+++ SistemaProven.AplicacionWeb/obj/Debug/net8.0/SistemaProven.AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaProven.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a614c531f78ce7a7fb46f24b146e47bde6c8d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1641ac209a35e40149c8f9a5a7ca0956221b319")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaProven.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaProven.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
